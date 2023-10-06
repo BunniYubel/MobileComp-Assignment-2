3655299d0488d58f12d3f2568c702b6435af18e0
BunniYubel <144637740+BunniYubel@users.noreply.github.com>
2023-10-06 18:51:22 +1100

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,19 +1,27 @@
-using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    void Start()
-    {
-        
-    }
+    public float movespeed;
+    public float horizontalBorder = 2.6f;
+    public float verticalBorder = 5f;
+    public Vector3 moveAmount;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+        moveAmount += new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * movespeed, Input.GetAxisRaw("Vertical") * Time.deltaTime * movespeed, 0);
+        Vector3 moveDiff = moveAmount * Time.deltaTime * 8;
+        transform.position += moveDiff;
+        moveAmount -= moveDiff;
+
+        if (transform.position.x < -horizontalBorder) transform.position = new Vector3(-horizontalBorder, transform.position.y, transform.position.z);
+        if (transform.position.x > horizontalBorder) transform.position = new Vector3(horizontalBorder, transform.position.y, transform.position.z);
+        if (transform.position.y < -verticalBorder) transform.position = new Vector3(transform.position.x, -verticalBorder, transform.position.z);
+        if (transform.position.y > verticalBorder) transform.position = new Vector3(transform.position.x, verticalBorder, transform.position.z);
     }
 }
