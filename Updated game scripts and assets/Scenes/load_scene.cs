71d270fe8e96453ea34cab9b812440ec9fd96a15
Amy Ly <119104583+amybok@users.noreply.github.com>
2023-11-03 20:44:02 +1100

    change name

---
+++ Updated game scripts and assets/Scenes/load_scene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Scene : MonoBehaviour
+{
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
