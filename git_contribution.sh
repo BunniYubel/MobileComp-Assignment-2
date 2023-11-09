97fbd38197eb02d1505aab508ee4b04a262d7dd5
RyougiShiki <53469345+ryougi-shiky@users.noreply.github.com>
2023-11-09 22:58:10 +0800

    add git log

--- git_contribution.sh
+++ git_contribution.sh
@@ -13,8 +13,8 @@ done
 # # Show number of lines, author name
 # git ls-files | while read f; do git blame -w -M -C -C --line-porcelain "$f" | grep -I '^author '; done | sort -f | uniq -ic | sort -n --reverse
 
-# # Show commit history detail
-# git log --format="author: %ae" --numstat
+# Show commit history detail
+git log --format="author: %ae" --numstat > unity_client_log.txt
 
 
 # -------------------------------------------------------------------------------
